b0ee88de6fd0d5e85c128591eb705f845b80529c
changjinqi1 <160363438+changjinqi1@users.noreply.github.com>
2025-03-21 14:14:28 -0700

    j
    2

--- My project/Assets/Scripts/IcePlatform.cs
+++ My project/Assets/Scripts/IcePlatform.cs
@@ -4,44 +4,35 @@ using System.Collections;
 public class IcePlatform : MonoBehaviour
 {
     [Header("Speed Boost Settings")]
-    public float speedMultiplier = 1.5f; // Multiplier for speed increase
-    public float boostDuration = 2f; // Duration of the speed boost in seconds
-
-    [Header("Player Settings")]
-    public float baseSpeed = 5f; // Player's normal speed
+    public float speedMultiplier = 1.5f; // Multiplier for orbitSpeed
+    public float boostDuration = 2f;     // Duration of the boost in seconds
 
     private Coroutine boostCoroutine;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        // Check if the player stepped on the platform
         if (collision.gameObject.CompareTag("Player"))
         {
-            Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            PlayerOrbitWithGravityAndCollision orbitScript = collision.gameObject.GetComponent<PlayerOrbitWithGravityAndCollision>();
 
-            if (playerRb != null)
+            if (orbitScript != null)
             {
-                // Stop any existing coroutine running on this script
                 if (boostCoroutine != null)
-                {
                     StopCoroutine(boostCoroutine);
-                }
 
-                boostCoroutine = StartCoroutine(SpeedBoost(playerRb));
+                boostCoroutine = StartCoroutine(SpeedBoost(orbitScript));
             }
         }
     }
 
-    private IEnumerator SpeedBoost(Rigidbody2D playerRb)
+    private IEnumerator SpeedBoost(PlayerOrbitWithGravityAndCollision playerOrbit)
     {
-        float boostedSpeed = baseSpeed * speedMultiplier;
+        float originalSpeed = playerOrbit.orbitSpeed;
 
-        // Apply boosted speed
-        playerRb.velocity = new Vector2(boostedSpeed * Mathf.Sign(playerRb.velocity.x), playerRb.velocity.y);
+        playerOrbit.orbitSpeed *= speedMultiplier;
 
         yield return new WaitForSeconds(boostDuration);
 
-        // Reset to base speed
-        playerRb.velocity = new Vector2(baseSpeed * Mathf.Sign(playerRb.velocity.x), playerRb.velocity.y);
+        playerOrbit.orbitSpeed = originalSpeed;
     }
 }
