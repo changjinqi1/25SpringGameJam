5b28d6a20828a7a3d2c732e08424881b0c1e0706
changjinqi1 <160363438+changjinqi1@users.noreply.github.com>
2025-03-21 14:05:16 -0700

    j
    2

--- My project/Assets/Scripts/Collect.cs
+++ My project/Assets/Scripts/Collect.cs
@@ -1,31 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections.Generic;
 
 public class Collect : MonoBehaviour
 {
-<<<<<<< HEAD
-    public Transform playerBase; // ��ҽ��µĻ�����
-    public float yarnHeight = 1f; // ÿ��ë����ĸ߶�
-    private List<GameObject> collectedYarnBalls = new List<GameObject>(); // �洢���ռ���ë����
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("YarnBall")) // ë���򣡣�������������Tag����������������������������������������
-        {
-            // ������Ҹ߶�
-            collectedYarnBalls.Add(other.gameObject);
-            UpdatePlayerHeight();
-
-            // ��ë�����ƶ�����ҽ���
-            PositionYarnBalls();
-
-            // �ر�ë���������Ӱ�죬�����������
-            Rigidbody rb = other.GetComponent<Rigidbody>();
-            if (rb != null) rb.isKinematic = true; // ������������Ӱ��
-            other.transform.SetParent(transform); // ��ë�����������ƶ�
-=======
     public Transform playerBase; // The base point under the player's feet
     public float yarnHeight = 1f; // The height of each ball of yarn
     private List<GameObject> collectedYarnBalls = new List<GameObject>(); // yarn balls list
@@ -47,7 +25,7 @@ public class Collect : MonoBehaviour
             Rigidbody rb = other.GetComponent<Rigidbody>();
             if (rb != null) rb.isKinematic = true; // Make it unaffected by gravity
             other.transform.SetParent(transform); // Make the ball of yarn follow the player
->>>>>>> 3edfeab975bd9214be53ce954970333e36e7671f
+
 
             Debug.Log("Collected Yarn Ball! Total: " + collectedYarnBalls.Count);
         }
@@ -55,22 +33,22 @@ public class Collect : MonoBehaviour
 
     void UpdatePlayerHeight()
     {
-<<<<<<< HEAD
+
         // ����ë��������������Ҹ߶�
-=======
+
         // Adjust the player's height based on the number of balls of yarn
->>>>>>> 3edfeab975bd9214be53ce954970333e36e7671f
+
         float newY = collectedYarnBalls.Count * yarnHeight;
         transform.position = new Vector3(transform.position.x, newY, transform.position.z);
     }
 
     void PositionYarnBalls()
     {
-<<<<<<< HEAD
+
         // ��������ë�����γ�һ����è��ߵ�Ч��
-=======
+
         // Arrange the balls of yarn in order to create an effect of raising the cat.
->>>>>>> 3edfeab975bd9214be53ce954970333e36e7671f
+
         for (int i = 0; i < collectedYarnBalls.Count; i++)
         {
             Vector3 newPos = playerBase.position + new Vector3(0, i * yarnHeight, 0);
@@ -78,20 +56,15 @@ public class Collect : MonoBehaviour
         }
     }
 
-<<<<<<< HEAD
-    // �Ƴ�ë����
-=======
+
     // Removing Yarn Balls
->>>>>>> 3edfeab975bd9214be53ce954970333e36e7671f
     public void RemoveYarnBall()
     {
         if (collectedYarnBalls.Count > 0)
         {
-<<<<<<< HEAD
-            //�������濪ʼ�Ƴ�
-=======
+
+            //
             //Remove from the bottom
->>>>>>> 3edfeab975bd9214be53ce954970333e36e7671f
             GameObject removedBall = collectedYarnBalls[0];
             collectedYarnBalls.RemoveAt(0);
             Destroy(removedBall);
