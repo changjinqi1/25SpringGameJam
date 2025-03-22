68abfb73f6693fdcd959c723b1c29934ab10daab
wzhao4 <zhaoweiting047@gmail.com>
2025-03-22 09:25:12 -0700

    LevelGenerator.cs and Trigger.cs

---
+++ My project/Assets/Scripts/LevelTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTrigger : MonoBehaviour
+{
+    private bool triggered = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (triggered) return;
+
+        if (other.CompareTag("Player"))
+        {
+            triggered = true;
+
+            // Get the StickPoint of the current level
+            Transform stickPoint = transform.root.Find("StickPoint");
+
+            // Let the player bind the new column
+            other.GetComponent<PlayerOrbitWithGravityAndCollision>().stick = stickPoint;
+
+            // Tell the LevelGenerator to generate the next level
+            FindObjectOfType<LevelGenerator>().SpawnNextLevel();
+        }
+    }
+}
